dd43b813b758be91325d71453bb8b7e0087a9744
Grant <gccody2010@live.com>
2023-01-04 18:17:06 -0500

    api_helper is now async

--- lib/apihelper/__init__.py
+++ lib/apihelper/__init__.py
@@ -1,7 +1,7 @@
 import datetime
 import urllib.parse
 
-import requests
+import aiohttp
 
 
 class ApiHelper:
@@ -9,59 +9,68 @@ class ApiHelper:
 
     def __init__(self, token: str, password: str):
         self.password = password
+        self.session = aiohttp.ClientSession()
         self.token = token
         self.headers = {'Authorization': f"{self.token}", 'Content-Type': 'application/json'}
 
-    def timeout_user(self, user_id: str, guild_id: str, minutes: int, reason: str):
+    async def timeout_user(self, user_id: str, guild_id: str, minutes: int, reason: str):
         endpoint = f'guilds/{guild_id}/members/{user_id}'
         url = self.BASE + endpoint
         timeout = (datetime.datetime.utcnow() + datetime.timedelta(minutes=minutes)).isoformat()
         json = {'communication_disabled_until': timeout, 'x-audit-log-reason': urllib.parse.quote(reason)}
-        session = requests.patch(url, json=json, headers=self.headers)
-        return session.status_code in range(200, 299), session.json()
+        session = await self.session.patch(url, json=json, headers=self.headers)
+        return session.status in range(200, 299), await session.json()
 
-    def remove_timeout(self, user_id: str, guild_id: str):
+    async def remove_timeout(self, user_id: str, guild_id: str):
         endpoint = f'guilds/{guild_id}/members/{user_id}'
         url = self.BASE + endpoint
         json = {'communication_disabled_until': None}
-        session = requests.patch(url, json=json, headers=self.headers)
-        return session.status_code in range(200,299), session.json()
+        session = await self.session.patch(url, json=json, headers=self.headers)
+        return session.status in range(200, 299), await session.json()
 
-    def edit_me(self, data):
-        url = "https://discord.com/api/v9/users/@me"
-        session = requests.patch(url, json=data, headers=self.headers)
-        return session.status_code in range(200, 299), session.json()
+    async def edit_me(self, data):
+        url = f"{self.BASE}/users/@me"
+        session = await self.session.patch(url, json=data, headers=self.headers)
+        return session.status in range(200, 299), await session.json()
 
-    def change_password(self, new: str, code: str | None):
+    async def change_password(self, new: str, code: str | None):
         data = {'password': self.password, 'new_password': new}
         if code: data['code'] = code
         res, data = self.edit_me(data)
         if res: self.password = new
         return res, data
 
-    def change_email(self, email: str, code: str | None):
+    async def change_email(self, email: str, code: str | None):
         data = {'email': email, "password": self.password}
         if code: data['code'] = code
-        return self.edit_me(data)
+        return await self.edit_me(data)
 
-    def set_username(self, username: str, discrim: str):
+    async def set_username(self, username: str, discrim: str):
         if len(discrim) != 4: raise ValueError("Discriminator must be a length of 4")
         data = {'discriminator': discrim, 'password': self.password, 'username': username}
-        return self.edit_me(data)
+        return await self.edit_me(data)
 
-    def remove_guild(self, guild_id: str, code: str | None):
-        url =  f"https://discord.com/api/v9/guilds/{guild_id}/delete"
+    async def remove_guild(self, guild_id: str, code: str | None):
+        url = f"{self.BASE}/guilds/{guild_id}/delete"
         data = {}
         if code: data['code'] = code
-        session = requests.post(url, json=data, headers=self.headers)
-        return session.status_code in range(200, 299), session.json()
+        session = await self.session.post(url, json=data, headers=self.headers)
+        return session.status in range(200, 299), await session.json()
 
-    def get_channels(self, guild_id: str):
-        url = f"https://discord.com/api/v9/guilds/{guild_id}/channels"
-        session = requests.get(url, headers=self.headers)
-        return [channel['id'] for channel in session.json()]
+    async def get_channels(self, guild_id: str):
+        url = f"{self.BASE}/guilds/{guild_id}/channels"
+        session = await self.session.get(url, headers=self.headers)
+        return [channel['id'] for channel in await session.json()]
 
-    def mfa_enabled(self):
-        url = "https://discord.com/api/v9/users/@me"
-        session = requests.get(url, headers=self.headers)
-        return session.json()['mfa_enabled']
+    async def mfa_enabled(self):
+        url = f"{self.BASE}/users/@me"
+        session = await self.session.get(url, headers=self.headers)
+        if session.status in range(200, 299):
+            return await session.json()['mfa_enabled']
+        else:
+            return await session.json()
+
+    async def token_info(self, token: str):
+        url = f"{self.BASE}/users/@me"
+        session = await self.session.get(url, headers={'Authorization': token, 'Content-Type': 'application/json'})
+        return await session.json()
